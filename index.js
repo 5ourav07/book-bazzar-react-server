4764abdb983bd5b0f81ab48042e1b7ea4183d58e
Sourav Saha <shanto.sourav07@gmail.com>
2022-11-25 12:30:02 +0600

    Initial Setup Done

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0nty043.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categoriesCollection = client.db('bookBazzar').collection('categories');
+
+        app.get('/categories', async (req, res) => {
+            const query = {}
+            const cursor = categoriesCollection.find(query);
+            const categories = await cursor.toArray();
+            res.send(categories);
+        });
+
+        app.get('/categories/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const categories = await categoriesCollection.findOne(query);
+            res.send(categories);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
+
+
+app.get('/', async (req, res) => {
+    res.send('Book Bazzar Server is Running...');
+})
+
+app.listen(port, () => console.log(`Book Bazzar Server is Running on ${port}`))
